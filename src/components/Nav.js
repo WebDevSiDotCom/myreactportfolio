69fad72acb3a1222ca530b4c45ae2efad37bf967
WebDevSiDotCom <96456502+WebDevSiDotCom@users.noreply.github.com>
2023-03-17 15:35:03 -0300

    Nav Updates
    Minor Nav updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,16 @@
 import React from "react";
+import "../styles.css";
 
 function Nav() {
     return (
         <header>
-            <h1 className="logo--text">Test</h1>
+            <h1 className="logo--text">Web Dev Si - Software Engineer</h1>
             <nav>
-                <a href="#about">About</a>
-                <a href="#projects">My Projects</a>
-                <a href="#contact">Contact Me</a>
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">My Projects</a></li>
+                    <li><a href="#contact">Contact Me</a></li>
+                </ul>
             </nav>
         </header>
     )

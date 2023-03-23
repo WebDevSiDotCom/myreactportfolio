9bed43006c8e172f54f23a682546eaea370fc57d
WebDevSiDotCom <96456502+WebDevSiDotCom@users.noreply.github.com>
2023-03-23 18:19:28 -0300

    Adding project card boilerplate
    Begin to create the project cards

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "../styles.css";
+
 
 function Main() {
     return (
         <main>
             <h2>My Projects</h2>
+            <div className="project-card">
+                <h3 className="project-card-title">Project Title</h3>
+                <p className="project-desc">Project description</p>
+            </div>
         </main>
     )
 }
